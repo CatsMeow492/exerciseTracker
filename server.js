11eae710b9f641293a60e339b788ba2f21181d2a
Taylor Mohney <taylormohneytech@gmail.com>
2023-04-01 10:57:16 -0700

    added untested routes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 // import env variables
 require('dotenv').config();
 const mongoUri = process.env.MONGODB_URI;
+const User = require('./models/user.model');
+const Exercise = require('./models/exercise.model');
 
 // Initialize app and set up port
 const app = express();
@@ -29,6 +31,87 @@ connection.once('open', () => {
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
   });
+// Add a new user
+app.post('/api/users', (req, res) => {
+    const username = req.body.username;
+
+    try {
+        const newUser = new User({ username });
+        newUser.save();
+        res.json({ username: newUser.username, _id: newUser._id });
+    } catch (error) {
+        res.status(400).json("Error: " + error);
+    }
+});
+
+app.post('/api/users/:userId/exercises', async (req, res) => {
+    const userId = req.params.userId;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = req.body.date ? new Date(req.body.date) : new Date();
+  
+    try {
+      const user = await User.findById(userId);
+  
+      if (!user) {
+        return res.status(400).json('Error: User not found');
+      }
+  
+      const newExercise = new Exercise({
+        username: user.username,
+        description,
+        duration,
+        date,
+      });
+  
+      await newExercise.save();
+  
+      res.json({
+        _id: user._id,
+        username: user.username,
+        date: newExercise.date.toDateString(),
+        duration: newExercise.duration,
+        description: newExercise.description,
+      });
+    } catch (error) {
+      res.status(400).json('Error: ' + error);
+    }
+  });
+
+  // Get a users exercise log
+app.get('/api/users/:userId/logs', async (req, res) => {
+    const userId = req.params.userId;
+    const fromDate = req.query.from ? new Date(req.query.from) : new Date(0);
+    const toDate = req.query.to ? new Date(req.query.to) : new Date();
+    const limit = req.query.limit ? parseInt(req.query.limit) : null;
+
+    try {
+        const user = await User.findById(userId);
+        // If user not found, return error
+        if (!user) {
+            return res.status(400).json('Error: User not found');
+        }
+
+        const log = await Exercise.find({
+            username: user.username,
+            date: { $gte: fromDate, $lte: toDate },
+        }).limit(limit);
+
+        const response = {
+            _id: user._id,
+            username: user.username,
+            count: log.length,
+            log: log.map((exercise) => ({
+                description: exercise.description,
+                duration: exercise.duration,
+                date: exercise.date.toDateString(),
+            })),
+        };
+        res.json(response);
+    } catch (error) {
+        res.status(400).json('Error: ' + error);
+    }
+});
 
 // Start the server
 const listener = app.listen(process.env.PORT || 3000, () => {
